d292a5abd42a395ea123e57261011ca6e8edbbbb
Ramon Prata <ramon.prata@dtidigital.com.br>
2019-04-22 19:34:08 -0300

    MODULO_VI

--- src/booksStore/bookStorePage.jsx
+++ src/booksStore/bookStorePage.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
-
+import { withStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+import { booksMock } from '../booksMock';
+import BookCard from './bookCard';
 class BookStorePage extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h1>BookStorePage</h1>
-      </div>
+      <Grid container className={classes.root}>
+        <Grid item xs={12} className={classes.booksContainer}>
+          <Grid container justify="center" spacing={24}>
+            {
+              booksMock.items.map(book => (
+              <Grid key={book.volumeInfo.title} item>
+                <BookCard book={book} />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
     );
   }
 }
 
-export default BookStorePage;
+const styles = {
+  root: {
+    flexGrow: 1,
+    padding: '0 10%'
+  },
+  booksContainer: {
+    paddingTop: '5%'
+  }
+};
+
+export default withStyles(styles)(BookStorePage);
